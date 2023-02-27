1cf70d1ab51dc7431fe91f81ec9c9efbc1daddb4
yasartaymaz <yasartaymaz@windowslive.com>
2023-02-27 09:37:58 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
 import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-    const [todos, setTodos] = useState([])
-    const todoNameRef = useRef()
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
 
-    useEffect(() => {
-        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-        if (storedTodos) setTodos(storedTodos)
-    }, [])
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
 
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-    }, [todos])
-
-    function toggleTodo(id) {
-        const newTodos = [...todos]
-        const todo = newTodos.find(todo => todo.id === id)
-        todo.complete = !todo.complete
-        setTodos(newTodos)
+  useEffect(() => {
+    if (todos.length !== 0) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
     }
+  }, [todos]);
 
-    function handleAddTodo(e) {
-        const todoValue = todoNameRef.current.value
-        const todoId = uuidv4()
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
 
-        if (todoValue === '') return
+  function handleAddTodo(e) {
+    const todoValue = todoNameRef.current.value;
+    const todoId = uuidv4();
 
-        setTodos(prevTodos => {
-            return [...prevTodos, { id: todoId, name: todoValue, complete: false }]
-        })
+    if (todoValue === "") return;
 
-        todoNameRef.current.value = null
-    }
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: todoId, name: todoValue, complete: false }];
+    });
 
-    function handleClearTodos() {
-        const newTodos = todos.filter(todo => !todo.complete)
-        setTodos(newTodos);
-    }
+    todoNameRef.current.value = null;
+  }
 
-    return (
-        <>
-            <TodoList todos={todos} toggleTodo={toggleTodo} />
-            <input ref={todoNameRef} type="text" />
-            <button onClick={handleAddTodo}>Add Todo</button>
-            <button onClick={handleClearTodos}>Clear Completed Todos</button>
-            <div>{todos.filter(todo => !todo.complete).length} left to do.</div>
-        </>
-    )
+  function handleClearTodos() {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed Todos</button>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do.</div>
+    </>
+  );
 }
 
 export default App;
